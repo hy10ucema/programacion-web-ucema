f0f6054783a4704d813774a83b2ef88e26df914b
santiagojferreyra <80840789+santiagojferreyra@users.noreply.github.com>
2021-09-13 23:32:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+    }
+    ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight
+    + "px";
+    })
